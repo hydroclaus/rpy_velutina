5f127ded4caba11b5aa53875f5e5bf4590440988
Claus Haslauer <claus.haslauer@iws.uni-stuttgart.de>
2025-12-02 15:09:26 +0100

    flash works

--- falsh0.py
+++ falsh0.py
@@ -1,5 +1,7 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 30)  # Adjust 30 to match your LED count
-pixels[0] = (255, 0, 0)  # Sets the first LED to red
+pixels = neopixel.NeoPixel(board.D10, 25)  # Adjust 30 to match your LED count
+pixels[0] = (255, 255, 255)  # Sets the first LED to red
+pixels[1] = (0, 255, 0)      # Sets the second LED to green
+pixels[2] = (0, 0, 255)      # Sets the third
