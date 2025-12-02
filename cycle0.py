6eb73a5a176fee8ca6cd346ae74bc2a22af218c0
ClausHaslauer <claus.haslauer@iws.uni-stuttgart.de>
2025-12-02 15:16:33 +0100

    added first cycle

---
+++ cycle0.py
@@ -0,0 +1,15 @@
+import time
+import board
+import neopixel
+
+pixels = neopixel.NeoPixel(board.D10, 25)  # Adjust 30 to match your LED count
+
+
+# Cycle through colors
+while True:
+    pixels.fill((255, 0, 0))  # Red
+    time.sleep(1)
+    pixels.fill((0, 255, 0))  # Green
+    time.sleep(1)
+    pixels.fill((0, 0, 255))  # Blue
+time.sleep(1)
