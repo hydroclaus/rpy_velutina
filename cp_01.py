1159b901c14a3e8892303eaa47b8113194db81b0
ClausHaslauer <claus.haslauer@iws.uni-stuttgart.de>
2025-12-04 13:59:19 +0100

    all blue

--- cp_01.py
+++ cp_01.py
@@ -8,5 +8,10 @@ numleds = 25
 bright = 1.0
 leds = adafruit_ws2801.WS2801(oclock, odata, numleds, brightness=bright, auto_write=False)
 
+# fills the first one blue
+# leds[0] = 0xff0000
+# leds.show()
+
+# fills all blue
 leds[0] = 0xff0000
 leds.show()
