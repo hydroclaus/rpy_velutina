5fb646a71e177c2524bafb1c156ef1a32b35407b
Claus Haslauer <claus.haslauer@iws.uni-stuttgart.de>
2025-12-04 13:53:51 +0100

    ttt

--- cp_01.py
+++ cp_01.py
@@ -5,7 +5,7 @@ odata = board.MOSI
 oclock = board.SCLK
 
 numleds = 25
-bright = 1.0
+bright = 0.5
 leds = adafruit_ws2801.WS2801(oclock, odata, numleds, brightness=bright, auto_write=False)
 
 leds[0] = 0xff0000
