1843d1c2052895327b25465fc35a4774602cb2f2
ClausHaslauer <claus.haslauer@iws.uni-stuttgart.de>
2025-12-04 13:57:18 +0100

    new circuitpython

---
+++ cp_01.py
@@ -0,0 +1,12 @@
+import board
+import adafruit_ws2801
+
+odata = board.MOSI
+oclock = board.SCLK
+
+numleds = 25
+bright = 1.0
+leds = adafruit_ws2801.WS2801(oclock, odata, numleds, brightness=bright, auto_write=False)
+
+leds[0] = 0xff0000
+leds.show()
