1ed8461f2600ffe613625851b1413949ff1d5983
CPH MM24 <claus.haslauer@iws.uni-stuttgart.de>
2026-05-14 09:19:50 +0200

    connecting to Wifi works

--- CO2_waveshare_OLED/src/scd30_01.py
+++ CO2_waveshare_OLED/src/scd30_01.py
@@ -3,47 +3,81 @@
 # SPDX-License-Identifier: Unlicense
 import time
 
+# for SCD-30
+try:
+    import board
+    import busio
+    import adafruit_scd30
+    import wifi
+except ImportError as exc:
+    raise RuntimeError(
+        'This script must run on a CircuitPython device with the wifi and adafruit_scd30 libraries installed.'
+    ) from exc
 
-import board
-import busio
+# for server (sending data)
+# todo: influxdb
 
-import adafruit_scd30
 
-# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
-# starting at 50KHz
-#i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
-i2c = busio.I2C(board.GP5, board.GP4, frequency=50000)
-scd = adafruit_scd30.SCD30(i2c)
+# for WLAN
+# Wi-Fi credentials
+SSID = 'Wlandrian'
+PASSWORD = 'wAnk-od-O-quaS-on-va'
 
-# Create output file with headers
-with open("/sensor_data.csv", "w") as fp:
-    fp.write("datetime,CO2 [PPM],Temp [C],relHum [%]\n")
 
-start_time = time.monotonic()
 
-while True:
-    # since the measurement interval is long (2+ seconds) we check for new data before reading
-    # the values, to ensure current readings.
-    if scd.data_available:
+def connect_wifi():
+    print(f'Connecting to Wi-Fi SSID: {SSID}')
+    wifi.radio.connect(SSID, PASSWORD)
 
-        
-        # Write to file
-        elapsed = time.monotonic() - start_time        
-        
-        #print("Data Available!")
-        print("time elapsed: seconds since start", elapsed)
-        print(f"    CO2: {scd.CO2:f} PPM")
-        print(f"    Temperature: {scd.temperature:0.2f} degrees C")
-        print(f"    Humidity: {scd.relative_humidity:0.2f} % rH")
-        print("")
-        print("     ... Waiting for new data...")
-        print("")
-        
-        
-        
-        with open("/sensor_data.csv", "a") as fp:
-            fp.write(f"{elapsed:.2f},{scd.CO2:f},{scd.temperature:0.2f},{scd.relative_humidity:0.2f}\n")
+    while not wifi.radio.ipv4_address:
+        time.sleep(0.1)
 
-    # 5 minutes in seconds
-    dt = 5 * 60.0
-    time.sleep(dt)
+    print('Connected to Wi-Fi')
+    print('IP address:', wifi.radio.ipv4_address)
+
+
+
+def gather_data():
+    # SCD-30 has tempremental I2C with clock stretching, datasheet recommends
+    # starting at 50KHz
+    #i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+    i2c = busio.I2C(board.GP5, board.GP4, frequency=50000)
+    scd = adafruit_scd30.SCD30(i2c)
+
+    # Create output file with headers
+    with open("/sensor_data.csv", "w") as fp:
+        fp.write("datetime,CO2 [PPM],Temp [C],relHum [%]\n")
+
+    start_time = time.monotonic()
+
+    while True:
+        # since the measurement interval is long (2+ seconds) we check for new data before reading
+        # the values, to ensure current readings.
+        if scd.data_available:
+
+            
+            # Write to file
+            elapsed = time.monotonic() - start_time        
+            
+            #print("Data Available!")
+            print("time elapsed: seconds since start", elapsed)
+            print(f"    CO2: {scd.CO2:f} PPM")
+            print(f"    Temperature: {scd.temperature:0.2f} degrees C")
+            print(f"    Humidity: {scd.relative_humidity:0.2f} % rH")
+            print("")
+            print("     ... Waiting for new data...")
+            print("")
+            
+            
+            
+            with open("/sensor_data.csv", "a") as fp:
+                fp.write(f"{elapsed:.2f},{scd.CO2:f},{scd.temperature:0.2f},{scd.relative_humidity:0.2f}\n")
+
+        # 5 minutes in seconds
+        dt = 5 * 60.0
+        time.sleep(dt)
+
+
+if __name__ == "__main__":
+    connect_wifi()
+    # gather_data()
