95650fde230d112596ba18cee6f2cd0f0927020e
CPH MM24 <claus.haslauer@iws.uni-stuttgart.de>
2026-05-14 10:26:00 +0200

    seems to work!
    YAY!

--- CO2_waveshare_OLED/src/scd30_websocket.py
+++ CO2_waveshare_OLED/src/scd30_websocket.py
@@ -1,8 +1,16 @@
 import asyncio
+import datetime
 
 # TCP server configuration
 SERVER_IP = "192.168.178.82"
 SERVER_PORT = 8080
+
+now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+with open(f'../data/sensor_data_{now}.csv', 'w') as fp:
+        fp.write('datetime,seconds_since_start,CO2 [PPM],Temp [C],relHum [%]\n')
+
+
 
 async def receive_temperature():
     reader, writer = await asyncio.open_connection(SERVER_IP, SERVER_PORT)
@@ -14,6 +22,8 @@ async def receive_temperature():
                 print("Server closed connection")
                 break
             print(f"Received: {data.decode().strip()}")
+            with open(f'../data/sensor_data_{now}.csv', 'a') as fp:
+                fp.write(f'{datetime.datetime.now()}, {data.decode().strip()}\n')
     finally:
         writer.close()
         await writer.wait_closed()
